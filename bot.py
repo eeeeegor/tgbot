3fc3ebd674749dd1259dd25c4b1fd64f74fd9889
eeeeegor <53909149+eeeeegor@users.noreply.github.com>
2022-05-01 20:41:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
 import telebot
+import sqlite3
+
+conn = sqlite3.connect('testing.db', check_same_thread=False)
+cursor = conn.cursor()
+
+name = cursor.execute('SELECT name FROM users').fetchall()
 
 bot = telebot.TeleBot('5258860993:AAFfdF8myYlunCrz8JaT-zws6Lpkejs0T4Q')
 @bot.message_handler(commands=["start"])
@@ -6,6 +12,8 @@ def start(m, res=False):
     bot.send_message(m.chat.id, 'Привет, как тебя зовут?)')
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, message.text + ', may1')
+    bot.send_message(message.chat.id, message.text + ', may1' + name)
 bot.polling(none_stop=True, interval=0)
+
+
 
